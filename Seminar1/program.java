d1476587cd350e2cf1566fd780c5c4892d674548
VladimirSokol <walldemarsokol@yandex.ru>
2022-12-08 23:04:11 +0300

    add seminar1

---
+++ Seminar1/program.java
@@ -0,0 +1,92 @@
+// public class program {
+//     public static void main(String[] args) {
+//         System.out.println("Bye world");
+//     }
+// }
+
+/**
+ * program
+ */
+// public class program {
+
+//     public static void main(String[] args) {
+//         short age = 10;
+//         int salary = 123456;
+//         System.out.println(age);
+//         System.out.println(salary);
+//     }
+// }
+// class Program
+// {
+//   public static void main(String[] args) {
+//       float e = 2.7f;
+//       double pi = 3.1415;
+//       System.out.println(e);        // 2.7
+//       System.out.println(pi);       // 3.1415
+//   }
+// }
+
+// class Program
+// {
+//   public static void main(String[] args) {
+//       char ch = '1';
+//       System.out.println(Character.isDigit(ch));  // true
+//       ch = 'a';
+//       System.out.println(Character.isDigit(ch));  // false
+//   }
+// }
+
+// public class program {
+//     public static void main(String[] args) {
+//         boolean flag1 = 123 <= 234;
+//         System.out.println(flag1);  // true
+//         boolean flag2 = 123 >= 234 || flag1;
+//         System.out.println(flag2);  // true
+//         boolean flag3 = flag1 ^ flag2;
+//         System.out.println(flag3);  // false
+//     }
+// }
+
+// public class program {
+//     public static void main(String[] args) {
+//         String msg ="Hello world";
+//         System.out.println(msg); // Hello world
+//     }
+// }
+
+// public class program {
+//     public static void main(String[] args) {
+//         var a = 123;
+//         System.out.println(a);  // 123
+//         var d = 123.456;
+//         System.out.println(d);  // 123.456
+//         System.out.println(getType(a));  // Integer
+//         System.out.println(getType(d));  // Double
+//         d = 1022;
+//         System.out.println(d);  // 1022
+//         //d = "mistake";
+//         //error: incompatible types:
+//         //String cannot be converted to double
+//     } 
+//     static String getType(Object o){
+//         return o.getClass().getSimpleName();
+//     }
+// }
+
+// class Program
+// {
+//   public static void main(String[] args) {
+//       System.out.println(Integer.MAX_VALUE); // 2147483647
+//       System.out.println(Integer.MIN_VALUE); // -2147483648
+//   }
+// }
+//Операторы
+// Присваивание: =
+
+// Арифметические: *, /, +, -, %, ++, --
+
+// Операции сравнения: <, >, ==, !=, >=, <=
+
+// Логические операции:  ||, &&, ^, !
+
+// Побитовые операции <<, >>, &, |, ^
