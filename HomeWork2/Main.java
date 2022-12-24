4182a45538e949ba16d9ab2cb98044e1a82f2e70
VladimirSokol <walldemarsokol@yandex.ru>
2022-12-24 19:10:17 +0300

    redact homework2

--- HomeWork2/Main.java
+++ HomeWork2/Main.java
@@ -43,10 +43,13 @@ class Solution {
     }
 
     private void dfs(int[][] image, int i, int j, boolean[][] seen, int startColor, int newColor) {
-        if (i < 0 || i == image.length || j < 0 || j == image[0].length)
+        if (i < 0 || i == image.length || j < 0 || j == image[0].length) {
             return;
-        if (image[i][j] != startColor || seen[i][j])
+        }
+
+        if (image[i][j] != startColor || seen[i][j]) {
             return;
+        }
 
         image[i][j] = newColor;
         seen[i][j] = true;
